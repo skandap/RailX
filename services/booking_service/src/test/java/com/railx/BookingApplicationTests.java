2e54bfaef8c8b4f8fca7a4ff2bf987d4ce4bfc12
SKANDA PRASAD <skandaprasad7@gmail.com>
2026-01-24 22:51:03 -1200

    booking and train service checkin

---
+++ services/booking_service/src/test/java/com/railx/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
